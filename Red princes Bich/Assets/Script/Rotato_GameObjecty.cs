6c732ee0838909c0801460dd1c9d28f661920717
mbayati2 <100643417+mbayati2@users.noreply.github.com>
2022-03-03 18:43:42 -0800

    dw

--- Red princes Bich/Assets/Script/Rotato_GameObjecty.cs
+++ Red princes Bich/Assets/Script/Rotato_GameObjecty.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,6 +11,19 @@ public class Rotato_GameObjecty : MonoBehaviour
     [SerializeField] float Forxe;
     [SerializeField] Vector3 up_dowm;
     [SerializeField] GameObject gamepir;
+    [SerializeField] bool picanle = false;
+    [Header("enemy hit back stuff")]
+    [SerializeField] Transform startpos = null;
+    [SerializeField] Transform endpos = null;
+    [SerializeField] float jourbeyTime = 1.0f;
+    [SerializeField] float speed;
+    [SerializeField] bool doit;
+    [SerializeField] bool yep;
+
+    float startTime;
+    Vector3 centerPoint;
+    Vector3 startRelCenter;
+    Vector3 endRelCenter;
     bool oneChance = true;
     void FixedUpdate()
     {
@@ -26,21 +40,61 @@ public class Rotato_GameObjecty : MonoBehaviour
      }
     private void Update()
     {
+        #if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.L)) { dioea(); }
+        #endif
+        GetCenter(Vector3.up);
+        if (doit)
+        {
+            startpos = transform;
+            float fracComlete = (Time.deltaTime - startTime) / jourbeyTime * speed;
+            transform.position = Vector3.Slerp(startRelCenter, endRelCenter, fracComlete * speed);
+            transform.position += centerPoint;
+        }
         if (!Input.GetMouseButtonDown(0)) { return; }
         if (oneChance == true)
         {
-            Rb.velocity = Vector2.zero;
+            if (Rb == null) { return; }
             Rb.AddForce(-transform.right * Forxe);
             gameObject.GetComponent<SpriteRenderer>().sprite = dead;
             Instantiate(gamepir , transform.position, Quaternion.identity);
             oneChance = false;
         }
     }
+
+    private void GetCenter(Vector3 up)
+    {
+        if (endpos == null) { endpos = GetTransform(); }
+        if (startpos == null) { startpos = Startpos(); }
+        centerPoint = (startpos.position + endpos.position) * 0.25f;
+        centerPoint -= up;
+        startRelCenter = startpos.position - centerPoint;
+        endRelCenter = endpos.position - centerPoint;
+    }
+    private Transform GetTransform()
+    {
+        return getrengerse.it.EndPOint;
+    }
+    private Transform Startpos()
+    {
+        return getrengerse.it.startpos;
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        if (other.gameObject.layer == LayerMask.NameToLayer("Ground") && picanle == false)
         {
             spiretSarte.intanse.gameObject.transform.position = this.transform.position + up_dowm;
+            dioea();
+            return;
+        }
+        if (other.tag == "Enemy")
+        {
+            Rb.velocity = Vector2.zero;
+            picanle = true;
+            doit = true;
+        }
+        if (other.tag == "Player" && picanle == true)
+        {
             dioea();
         }
     }
@@ -52,6 +106,7 @@ public class Rotato_GameObjecty : MonoBehaviour
     IEnumerator DIE()
     {
         yield return new WaitForSeconds(5);
+        if (picanle) { yield return new WaitForSeconds(5); }
         dioea();
     }
     private void dioea()
---
+++ Red princes Bich/Assets/getrengerse.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getrengerse : MonoBehaviour
+{
+    public static getrengerse it;
+    public Transform EndPOint;
+    public Transform startpos;
+
+    private void Awake()
+    {
+        it = this;
+    }
+}
