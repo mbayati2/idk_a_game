630749c4924cb4ab2fc21f846906c099514a6ec9
mbayati2 <100643417+mbayati2@users.noreply.github.com>
2022-03-12 06:03:47 -0800

    .,.

--- weWhantwarCraftLOL/Assets/Scripts/CameraMoment.cs
+++ weWhantwarCraftLOL/Assets/Scripts/CameraMoment.cs
@@ -4,14 +4,35 @@ using UnityEngine;
 public class CameraMoment : MonoBehaviour
 {
     [SerializeField] float panSpeed = 20f;
+    [SerializeField] float panBorderThickness = 10f;
+    [SerializeField] Vector3 player_pos;
+    [SerializeField] Transform Player;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = transform.position;
-        if (Input.GetKey("w"))
+        if (Input.mousePosition.y >= Screen.height - panBorderThickness)
         {
             pos.z += panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.y <= panBorderThickness)
+        {
+            pos.z -= panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            pos.x += panSpeed * Time.deltaTime;
+        }
+        if (Input.mousePosition.x <= panBorderThickness)
+        {
+            pos.x -= panSpeed * Time.deltaTime;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            player_pos = new Vector3(Player.position.x , transform.position.y, Player.position.z);
+            pos = player_pos;
+            Debug.Log("owo");
         }
 
         transform.position = pos;
