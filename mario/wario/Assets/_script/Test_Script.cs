d89718f567248bf1eac0ce71729cc241f1532da4
mbayati2 <100643417+mbayati2@users.noreply.github.com>
2022-06-13 06:31:05 -0700

    yu

--- mario/wario/Assets/_script/Test_Script.cs
+++ mario/wario/Assets/_script/Test_Script.cs
@@ -1,18 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Test_Script : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   float Tau = 6.28f;
+    [SerializeField] private float radiuse;
+    [SerializeField] private int Angeles_Amount;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private void OnDrawGizmosSelected() {
+       
+
+        Handles.DrawWireDisc(transform.position , transform.forward , radiuse);
+        for (int i = 0; i < Angeles_Amount; i++)
+        {
+            float Angels = i / Angeles_Amount * Tau;
+            float x = Mathf.Cos(Angels);
+            float y = Mathf.Sign(Angels);
+
+            Vector2 dir = new Vector2(x , y).normalized;
+
+            Handles.DrawLine(transform.position , (Vector2)transform.position + dir);
+        }
+
+   }
 }
